b624f9a014a2ea3f550d4849e580f63603d5b3ed
edwardliu567 <edwardliu567@gmail.com>
2025-11-25 00:08:51 -0500

    agregué el comentario hola

--- CapaPresentacion/inicioSesion.cs
+++ CapaPresentacion/inicioSesion.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using CapaNegocio;
 namespace CapaPresentacion
+    //hola
 {
     public partial class inicioSesion : Form
     {
--- CapaPresentacion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
+++ CapaPresentacion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b86ede7aff488edddbf553207437a5c6316bbe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
